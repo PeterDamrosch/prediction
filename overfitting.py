f6a383a0f1bd4af099912508c5ec9dea7c090e6f
PeterDamrosch <peter.damrosch@gmail.com>
2016-06-17 08:59:15 -0400

    Main added

--- overfitting.py
+++ overfitting.py
@@ -1,30 +1,32 @@
 import numpy as np
 import pandas as pd
 import statsmodels.formula.api as smf
+def main():
+	# Set seed for reproducible models
+	np.random.seed(414)
 
-# Set seed for reproducible models
-np.random.seed(414)
+	# Generate random dataset to play with
+	X = np.linspace(0, 15, 1000)
+	y = 3 * np.sin(X) + np.random.normal(1 + X, .2, 1000)
 
-# Generate random dataset to play with
-X = np.linspace(0, 15, 1000)
-y = 3 * np.sin(X) + np.random.normal(1 + X, .2, 1000)
+	# Split into training and testing datasets
+	train_X, train_y = X[:700], y[:700]
+	test_X, test_y = X[700:], y[700:]
+	train_df = pd.DataFrame({'X': train_X, 'y': train_y})
+	test_df = pd.DataFrame({'X': test_X, 'y': test_y})
 
-# Split into training and testing datasets
-train_X, train_y = X[:700], y[:700]
-test_X, test_y = X[700:], y[700:]
-train_df = pd.DataFrame({'X': train_X, 'y': train_y})
-test_df = pd.DataFrame({'X': test_X, 'y': test_y})
+	# Create a simple linear fit regression
+	linear_fit = smf.ols(formula='y ~ 1 + X', data=train_df).fit()
 
-# Create a simple linear fit regression
-linear_fit = smf.ols(formula='y ~ 1 + X', data=train_df).fit()
+	# Create a quadratic fit regression
+	quadratic_fit = smf.ols(formula='y ~ 1 + X + I(X**2)', data=train_df).fit()
 
-# Create a quadratic fit regression
-quadratic_fit = smf.ols(formula='y ~ 1 + X + I(X**2)', data=train_df).fit()
+	# Compare parameters
+	linear_fit_params = linear_fit.params
+	quadratic_fit_params = quadratic_fit.params
 
-# Compare parameters
-linear_fit_params = linear_fit.params
-quadratic_fit_params = quadratic_fit.params
-
-# Compare residuals
-linear_fit_residuals = linear_fit.df_resid
-quadratic_fit_residuals = quadratic_fit.df_resid
+	# Compare residuals
+	linear_fit_residuals = linear_fit.df_resid
+	quadratic_fit_residuals = quadratic_fit.df_resid
+if '__name__' == '__main__':
+	main()
